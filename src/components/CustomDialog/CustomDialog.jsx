294ffa60ba94480b8137404ab35118dbc9eb62c1
PRIYABRAT PANDA <121928016+PRIYABRAT0707@users.noreply.github.com>
2024-05-07 10:08:54 +0530

    drawer changes

--- src/components/CustomDialog/CustomDialog.jsx
+++ src/components/CustomDialog/CustomDialog.jsx
@@ -12,7 +12,7 @@ const CustomDialog = ({onClose,open,children,isFullScreen}) => {
   return (
     <React.Fragment>
          <Dialog fullScreen={isFullScreen}  open={open} onClose={onClose} TransitionComponent={Transition}>
-            {isFullScreen && (<Box sx={{p:1,display:"flex",alignItems:"center",justifyContent:"flex-end"}} ><CustomTooltips title={"CLOSE"}> <IconButton  aria-label="close" onClick={onClose} sx={{color: (theme) => theme.palette.grey[500]}} >
+            {isFullScreen && (<Box sx={{p:1,display:"flex",alignItems:"center",justifyContent:"flex-end"}} ><CustomTooltips title={"CLOSE"}> <IconButton  aria-label="close" onClick={onClose} sx={{color: (theme) => theme?.palette?.grey[500]}} >
           <IoClose /></IconButton> </CustomTooltips></Box>)}
         {children}
       </Dialog>
--- src/components/CustomDrawer/CustomDrawer.jsx
+++ src/components/CustomDrawer/CustomDrawer.jsx
@@ -1,10 +1,17 @@
-import { SwipeableDrawer } from '@mui/material'
+import { Box, IconButton, SwipeableDrawer } from '@mui/material'
 import React from 'react'
+import CustomTooltips from '../CustomTooltips/CustomTooltips'
+import { IoClose } from "react-icons/io5";
 
-const CustomDrawer = ({anchor,onClose,open,onOpen,children}) => {
+
+
+const CustomDrawer = ({anchor,onClose,open,onOpen,children,isCloseButtonRequired}) => {
   return (
     <React.Fragment>
        <SwipeableDrawer sx={{}} anchor={anchor} open={open} onClose={onClose} onOpen={onOpen}>
+       {isCloseButtonRequired && (<Box sx={{p:1,display:{xs:"flex",sm:"flex",md:"none",xl:"none",lg:"none"},alignItems:"center",justifyContent:"flex-end"}} ><CustomTooltips title={"CLOSE"}> <IconButton  aria-label="close" onClick={onClose} sx={{color: (theme) => theme?.palette?.grey[500]}} >
+          <IoClose /></IconButton> </CustomTooltips></Box>)}
+
         {children}
           </SwipeableDrawer>
     </React.Fragment>
